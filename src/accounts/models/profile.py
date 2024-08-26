1dc9c4964b2d655cda891e627a0f55486d03f0b2
RTMoo <toktarkazyramazan@gmail.com>
2024-08-26 15:29:17 +0500

    [feature-auth-2] Валидация номера, фото

--- src/accounts/models/profile.py
+++ src/accounts/models/profile.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from accounts.models.user import CustomUser
-
+from accounts.validators import validate_age, validate_image
+from accounts.models.city import City
 
 class Profile(models.Model):
-    user = models.OneToOneField(to=CustomUser, on_delete=models.CASCADE, verbose_name=_('Пользователь'), related_name='profile')
-    city_name = models.CharField(_('Город'), blank=True, null=True, max_length=100)
+    user = models.OneToOneField(to=CustomUser, on_delete=models.CASCADE, related_name='profile')
+    city = models.ForeignKey(to=City, on_delete=models.SET_NULL, null=True)
     public_name = models.CharField(_('Имя'), blank=True, null=True, max_length=100)
-    age = models.PositiveSmallIntegerField(_('Возраст'),blank=True, null=True, default=0)
-    photo_url = models.ImageField(_('Путь к фото'), blank=True, null=True)
-    description = models.TextField(_('Описание'))
-    hobbies = models.CharField(_('Хобби'), max_length=100)
+    age = models.PositiveSmallIntegerField(_('Возраст'),blank=True, null=True, default=0, validators=[validate_age])
+    photo_url = models.ImageField(_('Путь к фото'), blank=True, null=True, validators=[validate_image])
+    description = models.TextField(_('Описание'), blank=True, null=True)
+    hobbies = models.CharField(_('Хобби'), max_length=100, blank=True, null=True)
+
 
--- src/accounts/models/user.py
+++ src/accounts/models/user.py
@@ -3,12 +3,13 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from accounts.managers import CustomUserManager
-from accounts.validators import validate_password
+from accounts.validators import validate_password, validate_phone_number
 
 
 class CustomUser(AbstractUser):
     email = models.EmailField(_('Электронный адрес'), unique=True, max_length=255)
-    phone_number = models.CharField(_('Номер телефона'), unique=True, blank=True, max_length=20)
+    phone_number = models.CharField(_('Номер телефона'), unique=True, blank=True, max_length=20,
+                                    validators=[validate_phone_number])
     password = models.CharField(_('Пароль пользователя'), validators=[validate_password], max_length=255)
     code_word = models.CharField(_('Кодовое слово для входа'), max_length=12, blank=True)
 
