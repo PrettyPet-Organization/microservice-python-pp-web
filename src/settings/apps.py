5f6496141a9b41b5f8b1836dad4bf6de4b4e7892
NatsionalnoeDostoyanie <sanaflamenbaum@gmail.com>
2024-08-30 22:43:59 +0400

    fix-42-logging-2
    Путем кастомного фильтра исправлен вывод логов уровней DEBUG и INFO в консоль
    
    В check_system.py функция check_logs() вызывалась дважды - исправлено

--- src/settings/apps.py
+++ src/settings/apps.py
@@ -1,13 +1,13 @@
 # Настройки для регистрации приложений Django и их конфигураций
 
 INSTALLED_APPS = [
-    'accounts.apps.AuthsConfig',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'drf_spectacular',
-    'drf_spectacular_sidecar',
+    "accounts.apps.AuthsConfig",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "drf_spectacular",
+    "drf_spectacular_sidecar",
 ]
