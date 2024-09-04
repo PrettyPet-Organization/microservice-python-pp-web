2df823b5ff69958c035292907bf1175ab0c805c7
skolbasin <georgio1997@mail.ru>
2024-09-04 23:13:09 +0300

    добавил файл с фикстурами

---
+++ src/core/fixtures.py
@@ -0,0 +1,13 @@
+# Здесь находятся функции, создающие тестовые экземпляры модели определенных приложений
+
+def create_instance_common():
+    pass
+
+def create_instance_hackathons():
+    pass
+
+def create_instance_profiles():
+    pass
+
+def create_instance_projects():
+    pass
