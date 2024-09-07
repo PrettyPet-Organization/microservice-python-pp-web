a12b2c17b87cfe6c85ba981bbd0c561e99089a1c
RTMoo <toktarkazyramazan@gmail.com>
2024-09-07 22:54:00 +0500

    [feature-48-1] Added models for app projects

--- src/projects/models/participant_in_project_roles.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class ParticipantType(models.TextChoices):
-    CREATOR = 'creator', 'Creator'
-    ADMIN = 'admin', 'Admin'
-    PARTICIPANT = 'participant', 'Participant'
