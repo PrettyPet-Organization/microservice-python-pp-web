af85a9efe53fcc8982ae603167a757fb6216dcac
Edvvard0 <edward.osipchukzzz@gmail.com>
2024-09-16 19:41:35 +0300

    [feature-27]: добавил тест и логирование

--- src/common/tests.py
+++ src/common/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class RandomTimeViewTests(APITestCase):
+
+    def test_random_time_view(self):
+        response = self.client.get(reverse("random_time"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Check that the response contains the expected keys
+        self.assertIn("country", response.data)
+        self.assertIn("time", response.data)
--- src/common/views.py
+++ src/common/views.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime
 from random import choice
 
@@ -8,6 +9,9 @@ from django.utils.translation import gettext_lazy as _
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+# Creating a logger
+logger = logging.getLogger(__name__)
 
 
 class RandomTimeView(APIView):
@@ -24,12 +28,14 @@ class RandomTimeView(APIView):
     """
 
     def get(self, request: Request) -> Response:
+        logger.info("Request to get a random country and time in it.")
         random_country = choice(list(pycountry.countries))
         country_name = _(random_country.name)
         tz_name = pytz.timezone(
             pytz.country_timezones.get(random_country.alpha_2, [])[0]
         )
         current_time = datetime.now(tz_name)
+        logger.info(f"Now in {country_name}: {current_time.strftime('%H:%M:%S')}")
 
         return Response(
             {"country": country_name, "time": current_time.strftime("%H:%M:%S")}
