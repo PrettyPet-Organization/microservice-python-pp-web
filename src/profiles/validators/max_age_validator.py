4e1aa00f240f0402a5b40adc72bc3d5fb44c0790
NatsionalnoeDostoyanie <sanaflamenbaum@gmail.com>
2024-10-05 20:30:57 +0400

    [fix]: Мелкие исправления приложения profiles

--- src/profiles/validators/max_age_validator.py
+++ src/profiles/validators/max_age_validator.py
@@ -5,6 +5,7 @@ from django.core.validators import MaxValueValidator
 from django.utils.translation import gettext_lazy as _
 
 
+# TODO: Change this to something that uses the task scheduler to determine the oldest date based on the current date.
 class MaxAgeValidator(MaxValueValidator):
     """
     A validator that ensures that the user's (Profile's) age is less than or equal to a specified age limit.
